0b1615ee4f92f84476d270db5db16be3a16e8676
Cokke <JorgeRandomGamer@gmail.com>
2024-09-02 23:25:06 -0400

    initial endpoints & /graphql functional

--- app.js
+++ app.js
@@ -1,9 +1,41 @@
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql')
 
 const app = express();
 
 app.use(bodyParser.json());
+
+app.use(
+    '/graphql', 
+    graphqlHTTP({
+        schema: buildSchema(`
+            type RootQuery {
+                users: [String!]!
+            }
+
+            type RootMutation {
+                createUser(name: String): String
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            users () {
+                return ['Cokke', 'Remi', 'Ceci', 'Cris'];
+            },
+            createUser: (args) => {
+                const userName = args.name;
+                return userName;
+            }
+        },
+        graphiql: true
+    })
+);
 
 app.get('/', (req, res, next) => {
     res.send('Hello World!');
