a0911cfb554c288f0c7d2fa50f033b66527aa253
Cokke <JorgeRandomGamer@gmail.com>
2024-09-03 18:18:42 -0400

    back configure 01

---
+++ server/app.js
@@ -0,0 +1,71 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { graphqlHTTP } = require('express-graphql');
+const { buildSchema } = require('graphql')
+
+const app = express();
+
+const users = [];
+
+app.use(bodyParser.json());
+
+app.use(
+    '/graphql', 
+    graphqlHTTP({
+        schema: buildSchema(`
+            type User {
+                _id: ID!
+                name: String!
+                password: String!
+                age: Int!
+                email: String!
+                phone: String!
+            }
+
+            input UserInput {
+                name: String!
+                password: String!
+                age: Int!
+                email: String!
+                phone: String!
+            }
+
+            type RootQuery {
+                users: [User!]!
+            }
+
+            type RootMutation {
+                createUser(userInput: UserInput): User
+            }
+
+            schema {
+                query: RootQuery
+                mutation: RootMutation
+            }
+        `),
+        rootValue: {
+            users: () => {
+                return users;
+            },
+            createUser: (args) => {
+                const user = {
+                    _id: Math.random().toString(),
+                    name: args.userInput.name,
+                    password: args.userInput.password,
+                    age: +args.userInput.age,
+                    email: args.userInput.email,
+                    phone: args.userInput.phone
+                };
+                users.push(user);
+                return user;
+            }
+        },
+        graphiql: true
+    })
+);
+
+app.get('/', (req, res, next) => {
+    res.send('Hello World!');
+})
+
+app.listen(3000);
